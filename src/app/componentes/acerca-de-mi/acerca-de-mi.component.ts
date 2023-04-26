0c5fea16172d45bfd897249964acc90d3482dbe5
carlos gabrovich <carlos.gabrovich@gmail.com>
2023-04-26 10:30:03 -0300

    empiezo a usar el json para pasar los datos dinamicos al portfolio

--- src/app/componentes/acerca-de-mi/acerca-de-mi.component.ts
+++ src/app/componentes/acerca-de-mi/acerca-de-mi.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/servicios/datos.service';
 
 @Component({
   selector: 'app-acerca-de-mi',
   templateUrl: './acerca-de-mi.component.html',
   styleUrls: ['./acerca-de-mi.component.css']
 })
-export class AcercaDeMiComponent {
+export class AcercaDeMiComponent implements OnInit{
+  acercaDeMi:any;
+baner:any;
+  constructor(private datos:DatosService){}
 
+  ngOnInit(): void {
+    this.datos.getDatos().subscribe(data =>{
+    this.acercaDeMi=data.acercaDeMi;
+    
+    })
+  }
 }
--- src/app/componentes/modal-btn-login/modal-btn-login.component.ts
+++ src/app/componentes/modal-btn-login/modal-btn-login.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { DatosService } from 'src/app/servicios/datos.service';
 
 @Component({
   selector: 'app-modal-btn-login',
   templateUrl: './modal-btn-login.component.html',
-  styleUrls: ['./modal-btn-login.component.css']
+  styleUrls: ['./modal-btn-login.component.css'],
 })
-export class ModalBtnLoginComponent {
+export class ModalBtnLoginComponent implements OnInit {
+  user: any;
+  estado: Boolean = false;
+  pass = '';
+  useringresado = '';
 
+  constructor(private log: DatosService) {}
+
+  ngOnInit(): void {
+    this.log.getDatos().subscribe(datos => {
+      this.user = datos.user;
+    });
+  }
+
+  useringresed(event: Event) {
+    this.useringresado = (<HTMLInputElement>event.target).value;
+    if (this.useringresado == this.user.username) {
+      this.estado = true;
+    }
+  }
 }
